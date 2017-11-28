be1aa1c8fd60e8076a9b32a4bf0cacf65d081717
osboxes.org <osboxes@osboxes>
2017-11-28 13:30:19 -0500

    ajustes items de consumo

--- server/jogo/views.py
+++ server/jogo/views.py
@@ -1,9 +1,10 @@
-from jogo.models import Jogo
+from jogo.models import Jogo, JogoPerguntas
+from pergunta.models import Alternativa
 from user.models import Usuario
-from item.models import UsuarioItem
+from item.models import UsuarioItem, Item
 from jogo.serializers import JogoSerializer
 from user.serializers import UserStatusSerializer
-
+from pergunta.serializers import AlternativaSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView,UpdateAPIView
@@ -12,7 +13,7 @@ from user.helper import IsAdmin
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from django.core.exceptions import ObjectDoesNotExist
-import random as rand
+import random
 
 
 class JogoRecuperarCreateView(ListAPIView):
@@ -90,6 +91,9 @@ class JogoListView(ListAPIView):
 class JogoUsarItemView(UpdateAPIView):
     '''
         Consumo de itens durante o jogo
+        1 -> Carta
+        2 -> Pulo
+        3 -> Voto Popular
     '''
     permission_classes= (IsAuthenticated,)
     authentication_classes = (JSONWebTokenAuthentication,)
@@ -97,6 +101,15 @@ class JogoUsarItemView(UpdateAPIView):
     serializer_class = UserStatusSerializer
 
     def put(self, request, item_id, id_pergunta):
+        try:
+            id_JogoBD = Jogo.objects.filter(usuario_id=request.user.id,is_active=True).last().id
+            id_perguntaBD = JogoPerguntas.objects.filter(jogo_id=id_JogoBD).last().id_pergunta.id_pergunta
+        except ObjectDoesNotExist:
+            return Response(data={'Erro' : 'JogoID ou PerguntaID Invalidos.'}, status=status.HTTP_404_NOT_FOUND)
+
+        if int(id_perguntaBD)!=int(id_pergunta):
+            return Response(data={'Erro' : 'PerguntaID nao corresponde a pergunta atual.'}, status=status.HTTP_404_NOT_FOUND)
+
         item = Item.objects.get(pk=item_id)
         try:        
             usuarioitem = UsuarioItem.objects.get(usuario_id=request.user.id, item=item)
@@ -107,16 +120,47 @@ class JogoUsarItemView(UpdateAPIView):
         usuarioitem.save()
 
         if usuarioitem.qtde == 0:
-            usuarioitem.delete()
+            usuarioitem.delete()            
 
-        #atualiza a pergunta do momento
-        jogo = Jogo.objects.get(usuario_id=user.id,is_active=True)
-        jogo.acertos = jogo.acertos+1
-        jogo.save()
+        if item.nome == 'carta':
+            print 'carta'
+            cartas = random.randrange(0,3)
+            alternativas = Alternativa.objects.filter(id_pergunta=id_pergunta)
+            alt_res = list(alternativas.values_list('id_alternativa','resposta'))
+        
+            for i in range(0,cartas):
+                if int(alt_res[i][1])==0:
+                    alt_res.pop(i)
 
-        pontos = self.ObtemPontuacao(tempo)
-        user.pontos = user.pontos + pontos
-        user.save()
+            alter_fil = [alt_res[i][0] for i in range(0,len(alt_res))]
+            alternativas = alternativas.filter(id_alternativa__in=alter_fil)
+            return Response(AlternativaSerializer(alternativas,many=True).data,status=status.HTTP_200_OK)
 
-        user = Usuario.objects.get(pk=request.user.id)
-        return Response(UserStatusSerializer(user).data,status=status.HTTP_200_OK)
+
+        elif item.nome == 'voto popular':
+            print 'voto popular'
+
+            percErro = [random.randrange(10,30) for i in range(0,3)]
+            percCerto= 100-sum(percErro)
+            
+            alternativas = Alternativa.objects.extra(select={'percentual': 0}).filter(id_pergunta=id_pergunta)
+            alt_res = list(alternativas.values_list('percentual','id_alternativa','resposta','alternativa'))
+            alt_res2 = [list(alt_res[i]) for i in range(0,len(alt_res))]
+            
+            x  = 0
+            for i in range(0,len(alternativas)):
+                if int(alt_res[i][2])==0:
+                    alt_res2[i][0] = percErro[x]
+                    x += 1
+                else:
+                    alt_res2[i][0] = percCerto
+                    pass
+                
+            pass
+
+            data = [{'id_pergunta':id_pergunta, 'id_alternativa': i[1],'alternativa':i[3], 'resposta' : i[2], 'percentual': i[0]} for i in alt_res2]
+            return Response(data, status=status.HTTP_200_OK)
+
+        #elif item.nome == 'pulo': #nao faz nada, agora pode baixar outra questao
+
+        return Response(data={'Mensagem':'Pulo efetuado, pode chamar a proxima pergunta!'},status=status.HTTP_200_OK)
